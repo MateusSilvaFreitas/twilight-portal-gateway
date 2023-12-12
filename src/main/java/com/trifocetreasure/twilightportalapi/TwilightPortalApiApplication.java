ef888089248075974434dc8e237dddadae096ba8
Mateus Silva Freitas <mateus.freitas@paytrack.com.br>
2023-12-12 14:03:59 -0300

    Project structure

---
+++ src/main/java/com/trifocetreasure/twilightportalapi/TwilightPortalApiApplication.java
@@ -0,0 +1,13 @@
+package com.trifocetreasure.twilightportalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilightPortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilightPortalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trifocetreasure/twilightportalapi/TwilightPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trifocetreasure.twilightportalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilightPortalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
